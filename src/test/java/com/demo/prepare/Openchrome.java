c94a6e9b91eee3a70a3c55954aef09e86c89b310
amiteshCoder <amiteshshukla999@gmail.com>
2018-03-29 18:36:02 +0530

    try

---
+++ src/test/java/com/demo/prepare/Openchrome.java
@@ -0,0 +1,21 @@
+package com.demo.prepare;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Openchrome {
+
+	public static WebDriver driver;
+	
+	@Test
+	public static void browser() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "E:\\LIBRABRIES\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("http://www.qaclickacademy.com/practice.php");
+		driver.findElement(By.id("dropdown-class-example")).click();
+		Thread.sleep(3000);
+		driver.quit();
+	}
+}
